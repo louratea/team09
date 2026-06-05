a2a9d94fa6659546e3d9b2f02477125ba9633e69
Arsen <arsen.drozdov1@yandex.ru>
2026-06-05 12:41:07 +0000

    fixing app.py

--- backend/app.py
+++ backend/app.py
@@ -70,7 +70,7 @@ def add_note():
 
 @app.route('/api/health', methods=['GET'])
 def health_check():
-    return jsonify({"status": "broken-status"}), 200
+    return jsonify({"status": "healthy"}), 200
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
